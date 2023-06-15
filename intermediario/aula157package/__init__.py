b3a4a2ac61a1f40abbc94c1f2784c84f13ec9e4f
CPCabral <camiladeepaula@gmail.com>
2023-06-15 10:08:45 -0300

    package __init__

---
+++ intermediario/aula157package/__init__.py
@@ -0,0 +1,4 @@
+#arquivo de inicialização
+from .modulo import soma_do_modulo
+from .modulo2 import * #caso onde é aceitável
+print('Você importou: ', __name__)
---
+++ intermediario/aula158_init.py
@@ -0,0 +1,3 @@
+from aula157package import soma_do_modulo #com o init o caminho fica diferente
+
+print(soma_do_modulo(2,3))
