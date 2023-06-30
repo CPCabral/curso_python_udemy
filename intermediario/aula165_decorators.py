0cd9070d35a356d6eb1a56e722fa30a835170074
CPCabral <camiladeepaula@gmail.com>
2023-06-30 08:22:33 -0300

    introdução funções decoradoras

--- intermediario/aula165_decorators.py
+++ intermediario/aula165_decorators.py
@@ -1,8 +1,23 @@
 #Decorar = Adicionar/ Remover/ restringir / alterar
 #são funções que decoram outras funções
 
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        print('vou te decorar1')
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args,**kwargs)
+        print('Ok, agora vc foi decorada')
+        return resultado
+    return interna
+
 def inverte_string(string):
     return string[::-1]
 
-invertida = inverte_string('camila')
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError('param deve ser string')
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string_checando_parametro('123')
 print(invertida)
